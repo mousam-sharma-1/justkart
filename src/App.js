ca52faf725e8847751e54f76db387e9f4f4aa061
mousam sharma <mousam.sharma13@gmail.com>
2019-12-24 17:04:09 +0530

    24 dec

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import {Switch,Route} from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage.component';
+import SHOPPAGE from './pages/shop/shop.component'
 
 
 const Hatspage = () => (
   <div>
-    <h1>HATSPAGE</h1>
+    <h1>HATS PAGE</h1>
   </div>
 )
 
@@ -17,6 +18,7 @@ function App() {
       <Switch>
       <Route exact path="/" component={Homepage}></Route>
       <Route path="/hats" component={Hatspage}></Route>
+      <Route path="/shop" component={SHOPPAGE}></Route>
       </Switch>
     </div>
   );
---
+++ src/pages/shop/shop.data.js
@@ -0,0 +1,117 @@
+const SHOPDATA=[
+    {
+        id:1,
+        title: 'hats',
+        routerName: 'hats',
+        items: [
+            {
+                id: 1,
+                name:'Blue bean',
+                imageUrl: 'https://image.freepik.com/free-psd/_34168-1105.jpg',
+                price: 25                
+            },
+            {
+                id: 2,
+                name:'Brown bean',
+                imageUrl: 'https://image.freepik.com/free-psd/_34168-1105.jpg',
+                price: 25                
+            },
+            {
+                id: 3,
+                name:'Black bean',
+                imageUrl: 'https://image.freepik.com/free-psd/_34168-1105.jpg',
+                price: 25                
+            },
+            {
+                id: 4,
+                name:'Black bean',
+                imageUrl: 'https://image.freepik.com/free-psd/_34168-1105.jpg',
+                price: 25                
+            },
+            {
+                id: 5,
+                name:'Black bean',
+                imageUrl: 'https://image.freepik.com/free-psd/_34168-1105.jpg',
+                price: 25                
+            }
+        ]
+    },
+    {
+        id:2,
+        title: 'hats2',
+        routerName: 'hats',
+        items: [
+            {
+                id: 1,
+                name:'Blue bean',
+                imageUrl: 'https://image.freepik.com/free-psd/_34168-1105.jpg',
+                price: 25                
+            },
+            {
+                id: 2,
+                name:'Brown bean',
+                imageUrl: 'https://image.freepik.com/free-psd/_34168-1105.jpg',
+                price: 25                
+            },
+            {
+                id: 3,
+                name:'Black bean',
+                imageUrl: 'https://image.freepik.com/free-psd/_34168-1105.jpg',
+                price: 25                
+            },
+            {
+                id: 4,
+                name:'Black bean',
+                imageUrl: 'https://image.freepik.com/free-psd/_34168-1105.jpg',
+                price: 25                
+            },
+            {
+                id: 5,
+                name:'Black bean',
+                imageUrl: 'https://image.freepik.com/free-psd/_34168-1105.jpg',
+                price: 25                
+            }
+        ]
+    },
+    {
+        id:3,
+        title: 'hats3',
+        routerName: 'hats',
+        items: [
+            {
+                id: 1,
+                name:'Blue bean',
+                imageUrl: 'https://image.freepik.com/free-psd/_34168-1105.jpg',
+                price: 25                
+            },
+            {
+                id: 2,
+                name:'Brown bean',
+                imageUrl: 'https://image.freepik.com/free-psd/_34168-1105.jpg',
+                price: 25                
+            },
+            {
+                id: 3,
+                name:'Black bean',
+                imageUrl: 'https://image.freepik.com/free-psd/_34168-1105.jpg',
+                price: 25                
+            },
+            
+            {
+                id: 4,
+                name:'Black bean',
+                imageUrl: 'https://image.freepik.com/free-psd/_34168-1105.jpg',
+                price: 25                
+            },
+            {
+                id: 5,
+                name:'Black bean',
+                imageUrl: 'https://image.freepik.com/free-psd/_34168-1105.jpg',
+                price: 25                
+            }
+        ]
+    }
+
+]
+
+export default SHOPDATA;
